43fc22c7726696e7c7b4e7b7f7c25ae32f28ceeb
Brandon Burrus <brandon@burrus.io>
2021-08-18 23:17:43 -0700

    feat: implement client-side state persistence

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import { store } from "../redux/store";
+import { store, storePersistor } from "../redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Pane, Spinner } from "evergreen-ui";
 
 import "../styles/reset.css";
 import "../styles/globals.css";
@@ -8,8 +10,24 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate
+        loading={
+          <Pane
+            width="100vw"
+            height="100vh"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Spinner />
+          </Pane>
+        }
+        persistor={storePersistor}
+      >
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
+
 export default MyApp;
--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,14 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { itemsReducer } from "./items.reducer";
+import { persistReducer, persistStore } from "redux-persist";
+import createCompressor from "redux-persist-transform-compress";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 
+const rootReducer = combineReducers({
+  items: itemsReducer,
+});
+
+const middleware = [logger];
+
+const persistenceCompressor = createCompressor();
+
+const persistConfig = {
+  key: "RANDOM_PICKER",
+  version: 1,
+  timeout: 4_000,
+  storage,
+  transforms: [persistenceCompressor],
+};
+
 export const store = configureStore({
-  reducer: {
-    items: itemsReducer,
-  },
-  middleware: (currentMiddleware) => currentMiddleware().concat([logger]),
+  reducer: persistReducer(persistConfig, rootReducer),
+  middleware: (currentMiddleware) => currentMiddleware().concat(middleware),
   devTools: true,
 });
+
+export const storePersistor = persistStore(store);
 
 export type AppState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
